98c58d195a10c947181c39af92e03935814a2c55
Manik <msach22@gmail.com>
2018-01-29 08:26:58 -0800

    bump version & change log info

--- src/android/OpenTokAndroidPlugin.java
+++ src/android/OpenTokAndroidPlugin.java
@@ -740,7 +740,7 @@ public class OpenTokAndroidPlugin extends CordovaPlugin
                     JSONObject payload = new JSONObject();
                     try {
                         payload.put("platform", "Android");
-                        payload.put("cp_version", "3.1");
+                        payload.put("cp_version", "3.1.1");
                     } catch (JSONException e) {
                         Log.i(TAG, "Error creating payload json object");
                     }
