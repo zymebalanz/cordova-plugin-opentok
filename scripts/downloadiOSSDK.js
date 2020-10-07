737b00597e341b6a6739b099346fba73b0e617fc
zymebalanz <50338055+zymebalanz@users.noreply.github.com>
2020-10-06 22:48:22 -0700

    Update downloadiOSSDK.js

--- scripts/downloadiOSSDK.js
+++ scripts/downloadiOSSDK.js
@@ -4,7 +4,7 @@ module.exports = function (context) {
     var IosSDKVersion = "OpenTok-iOS-2.15.3";
     var downloadFile = require('./downloadFile.js'),
         exec = require('./exec/exec.js'),
-        Q = context.requireCordovaModule('q'),
+        Q = require('q'),
         deferral = new Q.defer();
     console.log('Downloading OpenTok iOS SDK');
     downloadFile('https://s3.amazonaws.com/artifact.tokbox.com/rel/ios-sdk/' + IosSDKVersion + '.tar.bz2',
